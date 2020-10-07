4680c6836fae4e1867e3f30a9c178601c1326da2
rolandadifan <rolandadifandana@gmail.com>
2020-10-07 08:38:38 +0700

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,47 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
+// import Product from "../views/Product.vue";
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+  },
+  {
+    path: "/about",
+    name: "About",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+  },
+  {
+    path: "/product",
+    name: "product",
+    component: () =>
+      import(/* webpackChunkName: "product" */ "../views/Product.vue"),
+  },
+  {
+    path: "/cart",
+    name: "ShoppingCart",
+    component: () =>
+      import(/* webpackChunkName: "cart" */ "../views/ShoppingCart.vue"),
+  },
+  {
+    path: "/success",
+    name: "Success",
+    component: () =>
+      import(/* webpackChunkName: "success" */ "../views/Success.vue"),
+  },
+];
+
+const router = new VueRouter({
+  routes,
+});
+
+export default router;
---
+++ src/views/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home">
+    <Header />
+    <Hero />
+    <Banner />
+    <Instagram />
+    <Patnerlogo />
+    <Footer />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Header from "@/components/Header.vue";
+import Hero from "@/components/Hero.vue";
+import Banner from "@/components/Banner.vue";
+import Instagram from "@/components/Instagram.vue";
+import Patnerlogo from "@/components/Patnerlogo.vue";
+import Footer from "@/components/Footer.vue";
+
+export default {
+  name: "Home",
+  components: {
+    Header,
+    Hero,
+    Banner,
+    Instagram,
+    Patnerlogo,
+    Footer,
+  },
+};
+</script>
