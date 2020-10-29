84c310aba35b7e722c2ed4c01efd4fceffc5f15a
Rolanda <46946118+rolandadifan@users.noreply.github.com>
2020-10-29 18:29:27 +0700

    new update

--- server.js
+++ server.js
@@ -4,9 +4,14 @@ const path = require('path')
 
 const app = express()
 
+//here we are configuring dist to serve app files
 app.use('/', serveStatic(path.join(__dirname, '/dist')))
 
-const port = process.env.PORT || 8000
-app.listen(port)
+// this * route is to serve project on different page routes except root `/`
+app.get(/.*/, function (req, res) {
+    res.sendFile(path.join(__dirname, '/dist/index.html'))
+})
 
-console.log('listening on port:' + port)
+const port = process.env.PORT || 8080
+app.listen(port)
+console.log(`app is listening on port: ${port}`)
